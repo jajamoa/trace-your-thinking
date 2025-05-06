0ddadf8c30176de385578daaf7dcc5b6a9df7fe5
LeeJAJA <jiajie@mit.edu>
2025-05-06 15:41:02 -0400

    added swc

--- next.config.mjs
+++ next.config.mjs
@@ -9,9 +9,15 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  swcMinify: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production' ? {
+      exclude: ['error', 'warn'],
+    } : false,
+  },
   webpack: (config, { isServer, dev }) => {
     if (!dev) {
-      config.optimization.minimizer.forEach((minimizer) => {
+      config.optimization.minimizer?.forEach((minimizer) => {
         if (minimizer.constructor.name === 'TerserPlugin') {
           minimizer.options.terserOptions.compress.drop_console = true;
         }
